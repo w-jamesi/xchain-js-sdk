900ceca2b1f47d671db47f23a02e515e4af80c36
51alisharper89 <smoggy-07-intent@icloud.com>
2025-07-09 18:57:07 +0200

    feat: multicall address (#302)

--- src/chains/evm/spoke/modules/folks-evm-loan.ts
+++ src/chains/evm/spoke/modules/folks-evm-loan.ts
@@ -5,6 +5,7 @@ import { TokenType } from "../../../../common/types/token.js";
 import { convertFromGenericAddress } from "../../../../common/utils/address.js";
 import { calcNextPeriodReset, calcPeriodNumber } from "../../../../common/utils/formulae.js";
 import { getGasLimitAfterIncrease } from "../../../../common/utils/messages.js";
+import { MULTICALL_ADDRESS } from "../../common/constants/address.js";
 import { getEvmSignerAccount } from "../../common/utils/chain.js";
 import { sendERC20Approve } from "../../common/utils/contract.js";
 import { getAllowanceStateOverride } from "../../common/utils/tokens.js";
@@ -720,6 +721,7 @@ export const read = {
         },
       ],
       allowFailure: false,
+      multicallAddress: MULTICALL_ADDRESS,
     });
 
     // TODO consider min limit
